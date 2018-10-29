f929131db081f74f5e224a9a07cf599d80c33186
Hoang Thi Linh <hoanglinh831999@gmail.com>
2018-10-29 07:38:06 +0700

    Create InOder_Traversal.cpp

---
+++ Tree Binary/InOder_Traversal.cpp
@@ -0,0 +1,67 @@
+/*
+1) Create an empty stack S.
+2) Initialize current node as root
+3) Push the current node to S and set current = current->left until current is NULL
+4) If current is NULL and stack is not empty then 
+     a) Pop the top item from stack.
+     b) Print the popped item, set current = popped_item->right 
+     c) Go to step 3.
+5) If current is NULL and stack is empty then we are done.
+*/
+#include <iostream>
+#include <stack> 
+using namespace std; 
+  
+/* A binary tree node has data, pointer to left child 
+and a pointer to right child */
+struct Node 
+{ 
+    int data; 
+    struct Node* left, *right; 
+    Node(int data) 
+    { 
+        this->data = data; 
+        left = right = NULL; 
+    } 
+}; 
+void inOrder(Node * root) {
+	 stack<Node *> s; 
+    Node *curr = root; 
+  
+    while (curr != NULL || !s.empty() ) 
+    { 
+        /* Reach the left most Node of the 
+           curr Node */
+        while (curr !=  NULL) 
+        { 
+            /* place pointer to a tree node on 
+               the stack before traversing 
+              the node's left subtree */
+            s.push(curr); 
+            curr = curr->left; 
+        } 
+  
+        /* Current must be NULL at this point */
+        curr = s.top(); 
+        s.pop(); 
+  
+        cout << curr->data << " "; 
+  
+        /* we have visited the node and its 
+           left subtree.  Now, it's right 
+           subtree's turn */
+        curr = curr->right; 
+  
+    } /* end of while */
+}
+int main() {
+	struct Node *root = new Node(1); 
+    root->left             = new Node(2); 
+    root->right         = new Node(3); 
+    root->left->left     = new Node(4); 
+    root->left->right = new Node(5);  
+  
+    cout << "\nInorder traversal of binary tree is \n"; 
+    inOrder(root); 
+    return 0;
+}
